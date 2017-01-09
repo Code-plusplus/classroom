572506bd6f98b911f36b5afc9a885d4f50f54689
Anik Raj <anikraj1994@gmail.com>
2017-01-10 02:44:54 +0530

    firebase post invert

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope, $http) {
-    $http.get('posts.json')
-        .then(function(res) {
-            $scope.posts = res.data;
-        });
+var app = angular.module("myApp", ["firebase"]);
+var database = firebase.database();
+
+app.controller("myCtrl", function($scope, $firebaseArray) {
+    var ref = firebase.database().ref().child('class/common/posts').limitToLast(20).orderByChild("sortDate");
+    // download the data into a local object
+    $scope.posts = $firebaseArray(ref);
+
 });
+
+function newPost() {
+    if (document.getElementById("newPost").value.trim() !== '') {
+        console.log(document.getElementById("newPost").value);
+        var newMessageRef = firebase.database().ref('class/common/posts').push();
+        newMessageRef.set({
+            'message': document.getElementById("newPost").value,
+            'user': {
+                'name': 'anikk',
+                'img': "asd"
+            },
+            createdDate: Date.now() + 0,
+            sortDate: 0 - Date.now()
+        }).then(function() {
+            document.getElementById("newPost").value = "";
+            toast("Posted!")
+        });
+    } else {
+        toast("Type something!")
+    }
+}
+
+function toast(x) {
+    'use strict';
+    var snackbarContainer = document.querySelector('#demo-toast-example');
+    var data = { message: x };
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+}
