fbf35e7f9b2e8c355704ab9a7146652ab809048a
Anik Raj <anikraj1994@gmail.com>
2017-01-11 00:05:12 +0530

    added user auth

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var app = angular.module("myApp", ["firebase"]);
 var database = firebase.database();
+getUser().then(function(result) {
+    console.log(result.displayName);
+}, function(err) {
+    console.log(err);
+    window.location = "/login.html"; // Error: "It broke"
+});
+//if (getUser() == null) window.location = "/login.html";
 
 app.controller("myCtrl", function($scope, $firebaseArray) {
     var ref = firebase.database().ref().child('class/common/posts').limitToLast(20).orderByChild("sortDate");
@@ -8,24 +15,54 @@ app.controller("myCtrl", function($scope, $firebaseArray) {
 
 });
 
-function newPost() {
-    if (document.getElementById("newPost").value.trim() !== '') {
-        console.log(document.getElementById("newPost").value);
-        var newMessageRef = firebase.database().ref('class/common/posts').push();
-        newMessageRef.set({
-            'message': document.getElementById("newPost").value,
-            'user': {
-                'name': 'anikk',
-                'img': "asd"
-            },
-            createdDate: Date.now() + 0,
-            sortDate: 0 - Date.now()
-        }).then(function() {
-            document.getElementById("newPost").value = "";
-            toast("Posted!")
+function getUser() {
+    return new Promise(function(resolve, reject) {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                console.log(user.displayName);
+                resolve(user);
+            } else {
+                // No user is signed in.
+                reject(Error("It broke"));
+            }
         });
+
+    });
+
+}
+
+function newPost() {
+    getUser().then(function(result) {
+        createPost(result);
+    }, function(err) {
+        console.log(err);
+        toast("User not logged in!");
+    });
+}
+
+function createPost(currentUser) {
+    console.log(currentUser.displayName);
+    if (currentUser != null) {
+        if (document.getElementById("newPost").value.trim() !== '') {
+            var newMessageRef = firebase.database().ref('class/common/posts').push();
+            newMessageRef.set({
+                'message': document.getElementById("newPost").value,
+                'user': {
+                    'name': currentUser.displayName,
+                    'img': currentUser.photoURL
+                },
+                createdDate: Date.now() + 0,
+                sortDate: 0 - Date.now()
+            }).then(function() {
+                document.getElementById("newPost").value = "";
+                toast("Posted!")
+            });
+        } else {
+            toast("Type something!");
+        }
     } else {
-        toast("Type something!")
+        toast("User not logged in!");
     }
 }
 
